0daddbb9c0a55192885841ad79714c72eccc04c9
Leao2020 <ricardobleao@hotmail.com>
2020-06-17 04:44:43 +0000

    Versão inicial em desenvolvimento

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './api';
 
 function App() {
-  return <h1>ReactJS, Template!</h1>
-}
+
+  const [lista, setLista] = useState([]); //imutabilidade
+
+  useEffect(() => {
+      api.get('/softwares').then((response) => {
+        const itens = response.data;
+        setLista(itens);
+    })
+}, [])
+
+
+    return (
+        <table>
+            {lista.map(item => (
+                <tr key={Item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.cliente}</td>
+                    <td>{item.versao}</td>
+                    <td>{item.versao}</td>
+                </tr>
+            ))}
+        </table>
+    );
+}    
 
 export default App;
