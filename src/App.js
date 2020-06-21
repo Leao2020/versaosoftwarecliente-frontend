13cf59943d00ccec8565ff1a283f90a5e23aaf2f
Leao2020 <ricardobleao@hotmail.com>
2020-06-21 07:32:22 +0000

    Versão quase pronta.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,110 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import api from './api';
-import Button from '@material-ui/core/Button';
-
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableRow from '@material-ui/core/TableRow';
+import Header from './header';
+import { 
+    Container, 
+    Table, 
+    TableRow, 
+    TableCell, 
+    Dialog, 
+    Button, 
+    DialogTitle, 
+    DialogContent, 
+    DialogContentText, 
+    TextField, 
+    DialogActions} from '@material-ui/core';
+import './style.css';
 
 function App() {
 
-  const [ lista, setLista ] = useState([]); //imutabilidade
-  const [ loading, setLoading ] = useState(true);
+    const [ lista, setLista ] = useState([]); // imutabilidade
+    const [ open, setOpen ] = useState(false);
+    const [ tarefa, setTarefa ] = useState('');
 
-  // executa para obter informações exernas  (construtor)
-  // "executado uma única vez"
-  useEffect(() => {
-      api.get('/softwares').then((response) => {
-        const itens = response.data;
-        setLista(itens);
-        setLoading(false);
-    })
-}, [])
+    function loadData() { 
+            api.get('/softwares').then((response) => {         // OK  
+            const itens = response.data;
+            setLista(itens);
+        });
+    }
 
+    useEffect(() => loadData(), [])
+
+    const openModal = () => setOpen(true);
+
+    // function closeModal() { setOpen(false); }
+    const closeModal = () => setOpen(false);
+
+    function addTarefa() { 
+         const name = tarefa;
+        
+            api.post('/softwares', { name: name }).then((response) => {
+            setTarefa('');
+            setOpen(false);
+            loadData();
+        })
+     }
+
+     function deleteTarefa(id) {                                           //OK  
+         api.delete(`/softwares/${id}`).then((response) => { 
+            loadData()
+         })
+     }
+    
 
     return (
-        <div style={{marginTop: '80px'}}>
-{ loading ? <span>Carregando dados...</span> : <div/> }
-        <Table>
-            <TableBody>
-            {lista.map(item => (
-                <TableRow key={item.id}>
-                    <TableCell>{item.id}</TableCell>
-                    <TableCell>{item.cliente}</TableCell>
-                    <TableCell>{item.versao}</TableCell>                    
-                </TableRow>
-            ))}
-            </TableBody>
-        </Table>
-        <br/>
-        {/*<Link to="/create">Adicionar</Link> */}
-        <Button variant="contained" color="primary">
-             Primary
-        </Button>     
-        </div>
+        <>
+        <Header />
+        <Container maxWidth="lg" className="container"> 
+            <Table>
+                {lista.map(item => (
+                    <TableRow key={item.id}>
+                        <TableCell>{item.id}</TableCell>
+                        <TableCell>{item.cliente}</TableCell>
+                        <TableCell>{item.app}</TableCell>
+                        <TableCell>{item.versao}</TableCell>
+                        <TableCell>
+                            <Button variant="outlined" size="small" color="secondary" onClick={() => deleteTarefa(item.id)}>Apagar</Button>
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </Table>
+            <Button 
+                onClick={openModal}
+                variant="contained" 
+                color="primary" 
+                style={{marginTop: '20px'}}>
+                Adicionar
+            </Button>
+        </Container>
+        <Dialog open={open} onClose={closeModal} fullWidth={true} maxWidth="sm">
+            <DialogTitle id="form-dialog-title">Nova Tarefa</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    Informe o Cliente, Software e Versão que deseja inserir.
+                </DialogContentText>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Digite as informações"
+                    type="email"
+                    fullWidth
+                    value={tarefa}
+                    onChange={e => setTarefa(e.target.value)}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={closeModal} color="primary">
+                    Cancelar
+                </Button>
+                <Button onClick={addTarefa} color="primary">
+                    Salvar
+                </Button>
+            </DialogActions>
+        </Dialog>
+        </>
     );
-}    
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+// import amber from '@material-ui/core/colors/amber';
 
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+const theme = createMuiTheme({
+  palette: {
+    primary: red,
+    // secondary: amber,
+  },  
+});
 
 ReactDOM.render(
-    <BrowserRouter>
-        <AppBar position="fixed" style={{marginBottom: '80px'}}>
-          <Toolbar>
-              <Typography variant="h6" >
-                 Versão de Software nos Clientes
-              </Typography>          
-          </Toolbar>
-      </AppBar>    
-       <Switch>
-            <Route path="/" exact>
-                <App/>
-            </Route>
-            <Route path="/create">
-                <h1>Cadastro</h1>
-            </Route>
-       </Switch>
-    </BrowserRouter> 
-
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>
 , document.getElementById('root'));
